35028387b4ae9e6c16599031194caddda6e526f5
Ryazatcev_Aleksei <ryazantcev1ry@gmail.com>
2025-07-08 18:21:10 +0300

    DB-Schema

--- src/main/java/com/app/userservice/models/User.java
+++ src/main/java/com/app/userservice/models/User.java
@@ -1,4 +1,59 @@
 package com.app.userservice.models;
 
+import jakarta.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String surname;
+
+    @Column(name = "birth_date")
+    @Temporal(TemporalType.DATE)
+    private Date birthday;
+
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
