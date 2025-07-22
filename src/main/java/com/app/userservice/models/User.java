c85c2c5dd6352de60175438ac3bb8d485896a74b
Ryazatcev_Aleksei <ryazantcev1ry@gmail.com>
2025-07-22 14:18:59 +0300

    DB-Schema

--- src/main/java/com/app/userservice/models/User.java
+++ src/main/java/com/app/userservice/models/User.java
@@ -2,10 +2,18 @@ package com.app.userservice.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.ToString;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
